6c488b89ca734dcef3a4fc4168d837b314a2e207
aldanabae <aldanatb@hotmail.com>
2017-02-02 18:14:36 -0300

    Inicio LOGIN

--- application/models/login_model.php
+++ application/models/login_model.php
@@ -5,28 +5,23 @@ class Login_model extends CI_Model {
 		parent::__construct();
 	}
 	
-	public function login($username,$password)
-	{
-   		$this->db-> select('idUsuario, usuario, contrasenia, nombreR, idNivel, tb_usuario.codResponsable');
-      $this->db-> where('usuario', $username);
-      $this->db-> where('contrasenia', MD5($password));
-   		$this->db-> from('tb_usuario');
-      $this->db->join('tb_responsable', 'tb_responsable.codResponsable = tb_usuario.codResponsable');
+	public function login($username,$password){
+   		$this->db->select('idUsuario, usuario, contrasenia, nombreE, idNivel, empleado.idEmpleado');
+      $this->db->where('usuario', $username);
+      $this->db->where('contrasenia', MD5($password));
+   		$this->db->from('usuario');
+      $this->db->join('empleado', 'empleado.idEmpleado = usuario.idEmpleado');
 
    		$this->db-> limit(1);
  
-   $query = $this->db-> get();
+      $query = $this->db-> get();
  
-   if($query -> num_rows() == 1)
-   {
-     return $query->result();
-    
-   }
-   else
-   {  	
-   	
-    	 return $query->result();
+      if($query -> num_rows() == 1){
+          return $query->result();
+      }else{  	
+   	      return $query->result();
 		}
 	}
 }
+
 ?>
