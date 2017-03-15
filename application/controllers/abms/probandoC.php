c7ff637727332ca09d87ba19bd7d95e315e03885
aldanabae <aldanatb@hotmail.com>
2017-03-15 18:02:48 -0300

    Probando preg y respuestas

---
+++ application/controllers/abms/probandoC.php
@@ -0,0 +1,24 @@
+<?php
+class ProbandoC extends My_Controller{
+
+	function __construct(){
+		parent::__construct();
+
+		$this->load->helper('form');
+		$this->load->helper('url');
+		$this->load->model('abms/probando_model'); 
+		$this->load->model('bienvenida_model'); 
+		$this->load->library('form_validation'); 
+	}
+
+	function index(){
+
+		
+		$data['respuestas'] = $this->probando_model->obtenerRespuestas();	
+		
+
+		$nombreVista="backend/abms/probando";
+		$this->cargarVista($nombreVista, $data);
+	}
+}
+?>
--- application/views/backend/abms/abmEmpleadosModificar.php
+++ application/views/backend/abms/abmEmpleadosModificar.php
@@ -166,16 +166,19 @@
 
 										<div class="clearfix form-actions"> <!-- Empiezan botones de modificar y limpiar -->
 											<div class="col-md-offset-3 col-md-9">
-												<button class="btn btn-info" type="submit" name="ActualizarEnDB">
-													<i class="ace-icon fa fa-check bigger-110"></i>
-													Modificar
-												</button>
-
+												<label>
+													<button class="btn btn-info" type="submit" name="ActualizarEnDB">
+														<i class="ace-icon fa fa-check bigger-110"></i>
+														Modificar
+													</button>
+												</label>
 												&nbsp; &nbsp; &nbsp;
-												<button class="btn" type="reset">
-													<i class="ace-icon fa fa-undo bigger-110"></i>
-													Limpiar
-												</button>
+												<label>
+													<button class="btn" type="reset">
+														<i class="ace-icon fa fa-undo bigger-110"></i>
+															Limpiar
+													</button>
+												</label>											
 											</div>
 										</div>
 
