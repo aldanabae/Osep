53efda3f9d25e84938b15634f83b47592e0902d8
aldanabae <aldanatb@hotmail.com>
2017-03-27 16:57:03 -0300

    Paso de encuesta a la vista listo!

--- application/controllers/abms/probandoC.php
+++ application/controllers/abms/probandoC.php
@@ -17,7 +17,7 @@ class ProbandoC extends My_Controller{
 		$data['respuestas'] = $this->probando_model->obtenerRespuestas();
 		$data['encuesta'] = $this->probando_model->obtenerEncuesta();
 		$data['bloques'] = $this->probando_model->obtenerBloques();	
-
+		//Variable que recibira la VISTA con todos los datos de la encuesta
 		$armoEncuesta = array('idBloque' =>"",
 								'nombreBloque' =>"",
 								'nroBloque' =>"",
@@ -30,80 +30,106 @@ class ProbandoC extends My_Controller{
 																				'respuesta' =>"")
 														)
 							);
+		//Contador de respuestas para armar cada $armoEncuesta
+		$cuentaResp = 0;
 
+		//Primero hago variar cada Bloque
 		foreach ($data['bloques'] as $bloq){
 			$idBloq = $bloq->idBloque;
 			$nombreBloq = $bloq->nombreBloque;
 			$nroBloq = $bloq->nroBloque;
-
+			
+			//Luego hago variar cada Pregunta de cada Bloque
 			foreach ($data['preguntas'] as $preg){
-				if($preg->idBloque == $idBloq){
+			 	if($preg->idBloque == $idBloq){
 					$idPreg = $preg->idPregunta;
 					$pgta = $preg->pregunta;
 					$idSubPreg = $preg->idSubPregunta;
 					$idTpoPreg = $preg->idTipoPregunta;
 					$idEtq = $preg->idEtiqueta;		
 					
-					foreach ($data['resp_preg'] as $respPreg){
-						if($respPreg->idPregunta == $idPreg){
-							$idResp = $respPreg->idRespuesta;
-							//Armar un array con todas las Resp-Preg que tienen esa pregunta para poder contarlos y hacer variar un loop 
-							//solo de la cantidad de respuestas que hay y ahi dentro crear cada $armoEncuesta
+					$contador = 0;
+					$arrayResp = array();
 
-							foreach ($data['respuestas'] as $rta){
-								if($rta->idRespuesta == $idResp){
-									$idRpta = $rta->idRespuesta;
-									$rpta = $rta->respuesta;
+					if($idTpoPreg==1 || $idTpoPreg==2 || $idTpoPreg==4){
 
-									$armoEncuesta['idBloque'] = $idBloq;
-									$armoEncuesta['nombreBloque'] = $nombreBloq;
-									$armoEncuesta['nroBloque'] = $nroBloq;
+						foreach ($data['resp_preg'] as $respPreg){	
+							if($respPreg->idPregunta == $idPreg){
+								$idResp = $respPreg->idRespuesta;
+								//Armar un array con todas las Resp-Preg que tienen esa pregunta para poder contarlos y hacer  
+								//variar un loop solo de la cantidad de respuestas que hay y ahi dentro crear cada $armoEncuesta
+								$arrayResp[$contador] = $idResp;
+								$contador++;
+							}	
+						}
 
-									$armoEncuesta['preguntas']['idPregunta']= $idPreg;
-									$armoEncuesta['preguntas']['pregunta'] = $pgta;
-									$armoEncuesta['preguntas']['idSubPregunta'] = $idSubPreg;
-									$armoEncuesta['preguntas']['idTipoPregunta'] = $idTpoPreg;
-									$armoEncuesta['preguntas']['idEtiqueta'] = $idEtq;
+						foreach ($arrayResp as $rta){
+							$idRta = $rta;
 
-									$armoEncuesta['preguntas']['respuestas']['idRespuesta'] = $idRpta;
-									$armoEncuesta['preguntas']['respuestas']['respuesta'] = $rpta;
+							foreach ($data['respuestas'] as $rtas){
+								if($rtas->idRespuesta == $idRta){
+									$idRpta = $rtas->idRespuesta;
+							 		$rpta = $rtas->respuesta;
+
+							 		$armoEncuesta[$cuentaResp]['idBloque'] = $idBloq;
+									$armoEncuesta[$cuentaResp]['nombreBloque'] = $nombreBloq;
+									$armoEncuesta[$cuentaResp]['nroBloque'] = $nroBloq;
+
+									$armoEncuesta[$cuentaResp]['preguntas']['idPregunta']= $idPreg;
+									$armoEncuesta[$cuentaResp]['preguntas']['pregunta'] = $pgta;
+									$armoEncuesta[$cuentaResp]['preguntas']['idSubPregunta'] = $idSubPreg;
+									$armoEncuesta[$cuentaResp]['preguntas']['idTipoPregunta'] = $idTpoPreg;
+									$armoEncuesta[$cuentaResp]['preguntas']['idEtiqueta'] = $idEtq;
+
+									$armoEncuesta[$cuentaResp]['preguntas']['respuestas']['idRespuesta'] = $idRpta;
+									$armoEncuesta[$cuentaResp]['preguntas']['respuestas']['respuesta'] = $rpta;
+
+									$cuentaResp++;
 								}
 							}
 						}
-					}
+					}else{
+						$armoEncuesta[$cuentaResp]['idBloque'] = $idBloq;
+						$armoEncuesta[$cuentaResp]['nombreBloque'] = $nombreBloq;
+						$armoEncuesta[$cuentaResp]['nroBloque'] = $nroBloq;
+
+						$armoEncuesta[$cuentaResp]['preguntas']['idPregunta']= $idPreg;
+						$armoEncuesta[$cuentaResp]['preguntas']['pregunta'] = $pgta;
+						$armoEncuesta[$cuentaResp]['preguntas']['idSubPregunta'] = $idSubPreg;
+						$armoEncuesta[$cuentaResp]['preguntas']['idTipoPregunta'] = $idTpoPreg;
+						$armoEncuesta[$cuentaResp]['preguntas']['idEtiqueta'] = $idEtq;
+
+						$armoEncuesta[$cuentaResp]['preguntas']['respuestas']['idRespuesta'] = NULL;
+						$armoEncuesta[$cuentaResp]['preguntas']['respuestas']['respuesta'] = NULL;
+
+						$cuentaResp++;
+					}	
 				}
-			}
-		}
-		
-		if (isset($armoEncuesta)){
-			var_dump($armoEncuesta);
-			echo "</br>";
-			echo "</br>";
-			echo count($armoEncuesta);
-			echo "</br>";
-			echo "</br>";
-			echo count($armoEncuesta, COUNT_RECURSIVE);
-		}
+			} 
+		} 
 
+		echo $cuentaResp;
+		echo "</br>";
+		echo "idBloque".$armoEncuesta[241]['idBloque'];
 		echo "</br>";
 		echo "</br>";
-		$armoEncuesta[0]['idBloque'] = 1;
-		$armoEncuesta[1]['idBloque'] = 2;
-		$armoEncuesta[2]['idBloque'] = 3;
-
-		echo $armoEncuesta[1]['idBloque'];
+		echo "idPregunta".$armoEncuesta[241]['preguntas']['idPregunta'];
 		echo "</br>";
 		echo "</br>";
-		echo $armoEncuesta['preguntas']['respuestas']['idRespuesta'];
+		echo "pregunta".$armoEncuesta[241]['preguntas']['pregunta'];
 		echo "</br>";
 		echo "</br>";
-		echo $armoEncuesta['preguntas']['respuestas']['respuesta'];
-		
-		
+		echo "idRespuesta".$armoEncuesta[241]['preguntas']['respuestas']['idRespuesta'];
+		echo "</br>";
+		echo "</br>";
+		echo "respuesta".$armoEncuesta[241]['preguntas']['respuestas']['respuesta'];
+		echo "</br>";
+		echo "</br>";
 		die();
-
+		
+		
 		$nombreVista="backend/abms/probando";
 		$this->cargarVista($nombreVista, $data);
 	}
-}
+ }
 ?>
