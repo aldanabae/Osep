cdf07498f9f51787bfaf28eab39428b17cd9dbe4
aldanabae <aldanatb@hotmail.com>
2017-04-05 14:51:37 -0300

    Commit entrega 05 abril

--- application/views/backend/encuesta/cargar_encuesta_inicio_view.php
+++ application/views/backend/encuesta/cargar_encuesta_inicio_view.php
@@ -153,3 +153,13 @@
 
 
 <!--Para que se vean los botones de la tabla responsive-->
+
+        <script type="text/javascript">
+            window.jQuery || document.write("<script src='../../assets/js/jquery.js'>"+"<"+"/script>");
+        </script>
+
+
+        <script type="text/javascript">
+            if('ontouchstart' in document.documentElement) document.write("<script src='../../assets/js/jquery.mobile.custom.js'>"+"<"+"/script>");
+        </script>
+        <script src="../../assets/js/bootstrap.js"></script>
