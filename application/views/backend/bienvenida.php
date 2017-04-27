659d94bbcaa734b5d572323718c74b4a7d994ab7
aldanabae <aldanatb@hotmail.com>
2017-04-27 16:22:15 -0300

    Boton Filtrar cant elemntos tabla terminado

--- application/views/backend/bienvenida.php
+++ application/views/backend/bienvenida.php
@@ -8,11 +8,11 @@
 <!--Para que se vean los botones de la tabla responsive-->
 
 <script type="text/javascript">
-	window.jQuery || document.write("<script src='assets/js/jquery.js'>"+"<"+"/script>");
+	window.jQuery || document.write("<script src='../assets/js/jquery.js'>"+"<"+"/script>");
 </script>
 
 
 <script type="text/javascript">
-	if('ontouchstart' in document.documentElement) document.write("<script src='assets/js/jquery.mobile.custom.js'>"+"<"+"/script>");
+	if('ontouchstart' in document.documentElement) document.write("<script src='../assets/js/jquery.mobile.custom.js'>"+"<"+"/script>");
 </script>
-<script src="assets/js/bootstrap.js"></script>
+<script src="../assets/js/bootstrap.js"></script>
